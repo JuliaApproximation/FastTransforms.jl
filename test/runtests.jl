32ae4ddc57176fbc3cdd59cd20faf64344d9068f
Michael Clarke <mike.a.clarke93@gmail.com>
2016-10-13 19:05:50 +1100

    Add PadTransform tests to runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -205,3 +205,27 @@ c = randn(1000)./√(1:1000);
 @test norm(jac2jac(c,0.,√2/2,-1/4,√2/2)-jjt(c,0.,√2/2,-1/4,√2/2),Inf) < 10length(c)*eps()
 
 @test norm(ultra2ultra(ultra2ultra(c,.5,.75),.75,.5)-c,Inf) < 10length(c)*eps()
+
+println("Testing Pad Transforms and their inverse function property")
+n=100
+N=div((n+1)*(n+2),2)
+v=rand(N) # Length of v is the no. of Padua points
+@test norm(padtransform(ipadtransform(v))-v)< 100eps()
+@test norm(ipadtransform(padtransform(v))-v)< 100eps()
+@test_approx_eq padtransform(ipadtransform(v)) v
+@test_approx_eq ipadtransform(padtransform(v)) v
+
+println("Testing runtimes for Pad Transforms")
+@time padtransform(v)
+@time ipadtransform(v)
+
+println("Accuracy of 2d function interpolation at a point")
+f = (x,y) -> x^2*y+x^3
+g = (x,y) ->cos(exp(2*x+y))*sin(y)
+x=0.1;y=0.2
+m=20
+l=80
+f_x=padeval(f,x,y,m)
+g_x=padeval(g,x,y,l)
+@test_approx_eq f(x,y) f_x
+@test_approx_eq g(x,y) g_x
