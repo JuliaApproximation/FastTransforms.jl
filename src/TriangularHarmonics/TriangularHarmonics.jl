c4e41cfb5a186ba1cdbc77773dcf33526e6fdc57
Sheehan Olver <solver@mac.com>
2018-08-02 20:04:54 +0100

    Just use slow triangular transform until fast is implemented

--- src/TriangularHarmonics/TriangularHarmonics.jl
+++ src/TriangularHarmonics/TriangularHarmonics.jl
@@ -13,11 +13,11 @@ include("slowplan.jl")
 
 function plan_tri2cheb(A::AbstractMatrix, α, β, γ; opts...)
     M, N = size(A)
-    if M ≤ 1023
+    # if M ≤ 1023
         SlowTriangularHarmonicPlan(A, α, β, γ)
-    else
-        ThinTriangularHarmonicPlan(A, α, β, γ; opts...)
-    end
+    # else
+    #     ThinTriangularHarmonicPlan(A, α, β, γ; opts...)
+    # end
 end
 
 tri2cheb(A::AbstractMatrix, α, β, γ; opts...) = plan_tri2cheb(A, α, β, γ; opts...)*A
