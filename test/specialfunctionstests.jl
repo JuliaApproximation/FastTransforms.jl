ed9f733dfa1107eeef83dcde6ea9e788bde3b1be
putianyi889 <44583944+putianyi889@users.noreply.github.com>
2019-06-15 21:30:45 +0100

    Update specialfunctionstests.jl

--- test/specialfunctionstests.jl
+++ test/specialfunctionstests.jl
@@ -4,5 +4,5 @@ using Compat.Test
 @testset "Pochhammer" begin
     @test pochhammer(2,3) == 24
     @test pochhammer(0.5,3) == 0.5*1.5*2.5
-    @test pochhammer(0.5,0.5) == 1/sqrt(pi)
+    @test pochhammer(0.5,0.5) == 1/sqrtpi
 end
