c140788cda6ce9dd3879487cab4166c6aa3dd101
putianyi889 <44583944+putianyi889@users.noreply.github.com>
2019-06-15 21:39:37 +0100

    Update specialfunctionstests.jl

--- test/specialfunctionstests.jl
+++ test/specialfunctionstests.jl
@@ -5,4 +5,9 @@ using Compat.Test
     @test pochhammer(2,3) == 24
     @test pochhammer(0.5,3) == 0.5*1.5*2.5
     @test pochhammer(0.5,0.5) == 1/sqrtpi
+    @test pochhammer(0,1) == 0
+    @test pochhammer(-1,2) == 0
+    @test pochhammer(-5,3) == -60
+    @test pochhammer(-1,-0.5) == 0
+    @test pochhammer(-0.5,-0.5) == Inf
 end
