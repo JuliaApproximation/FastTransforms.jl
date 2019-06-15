cdb83ddac026541fe9e1663ec2730e16c82535be
putianyi889 <44583944+putianyi889@users.noreply.github.com>
2019-06-15 18:29:29 +0100

    Update specialfunctionstests.jl

--- test/specialfunctionstests.jl
+++ test/specialfunctionstests.jl
@@ -1,5 +1,8 @@
 using FastTransforms
+using Compat.Test
 
 @testset "Pochhammer" begin
-
+    @test pochhammer(2,3) == 24
+    @test pochhammer(0.5,3) == 0.5*1.5*2.5
+    @test pochhammer(0.5,0.5) == 1/sqrt(pi)
 end
