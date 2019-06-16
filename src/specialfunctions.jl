8d1839b398fd5b35b840522ba3c6b5c1aacbd0d2
putianyi889 <44583944+putianyi889@users.noreply.github.com>
2019-06-16 19:30:42 +0100

    Update specialfunctions.jl

--- src/specialfunctions.jl
+++ src/specialfunctions.jl
@@ -50,7 +50,7 @@ pochhammer(x::AbstractArray{T,2},n::Integer) where {T<:Number} = [pochhammer(x[i
 pochhammer(x::AbstractArray{T},n::Integer) where {T<:Number} = reshape([ pochhammer(x[i],n) for i in eachindex(x) ], size(x))
 
 pochhammer(x::Number,n::Number) = isinteger(n) ? pochhammer(x,Int(n)) : ogamma(x)/ogamma(x+n)
-pochhammer(x::AbstractArray{T},n::Number) where {T<:Number} = isinteger(n) ? pochhammer(x,Int(n)) : ogamma.(x)./newgamma.(x.+n)
+pochhammer(x::AbstractArray{T},n::Number) where {T<:Number} = isinteger(n) ? pochhammer(x,Int(n)) : ogamma.(x)./ogamma.(x.+n)
 
 function pochhammer(x::Number,n::UnitRange{T}) where T<:Real
     ret = Vector{promote_type(typeof(x),T)}(length(n))
