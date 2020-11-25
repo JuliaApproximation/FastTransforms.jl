04008f6e863c7879e03b2bf345f0b8ec4023c6c2
MikaelSlevinsky <richard.slevinsky@umanitoba.ca>
2020-11-25 17:14:57 -0600

    better point sizes

--- examples/disk.jl
+++ examples/disk.jl
@@ -41,8 +41,8 @@ F = [f(r*cospi(θ), r*sinpi(θ)) for r in r, θ in θ]
 # We superpose a surface plot of $f$ on top of the grid:
 X = [r*cospi(θ) for r in r, θ in θ]
 Y = [r*sinpi(θ) for r in r, θ in θ]
-scatter3d(vec(X), vec(Y), vec(0X); markercolor=:red, size=(1000, 1000))
-surface!(X, Y, F; legend=false, xlabel="x", ylabel="y")
+scatter3d(vec(X), vec(Y), vec(0X); markersize=0.75, markercolor=:red, size=(1000, 1000))
+surface!(X, Y, F; legend=false, xlabel="x", ylabel="y", zlabel="f")
 
 # We precompute a (generalized) Zernike--Chebyshev×Fourier plan:
 α, β = 0, 0
@@ -95,8 +95,8 @@ F = [f(x[n], w[n]*z) for n in 1:N, z in z]
 # We superpose a surface plot of $f$ on top of the grid:
 X = [x for x in x, z in z]
 Y = [w*z for w in w, z in z]
-scatter3d(vec(X), vec(Y), vec(0X); markercolor=:red, size=(1000, 1000))
-surface!(X, Y, F; legend=false, xlabel="x", ylabel="y")
+scatter3d(vec(X), vec(Y), vec(0X); markersize=0.75, markercolor=:green, size=(1000, 1000))
+surface!(X, Y, F; legend=false, xlabel="x", ylabel="y", zlabel="f")
 
 # We precompute a Dunkl-Xu--Chebyshev plan:
 P = plan_rectdisk2cheb(F, β)
