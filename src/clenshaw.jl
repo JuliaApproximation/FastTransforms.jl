062f15eda9e8b21c4f2d391e87f3b8e3e3ae753e
Sheehan Olver <solver@mac.com>
2021-09-30 20:45:10 +0100

    add docs

--- src/clenshaw.jl
+++ src/clenshaw.jl
@@ -49,6 +49,9 @@ evaluates the orthogonal polynomial expansion with coefficients `c` at points `x
 where `A`, `B`, and `C` are `AbstractVector`s containing the recurrence coefficients
 as defined in DLMF,
 overwriting `x` with the results.
+
+If `c` is a matrix this treats each column as a separate vector of coefficients, returning a vector
+if `x` is a number and a matrix if `x` is a vector.
 """
 clenshaw!(c::AbstractVector, A::AbstractVector, B::AbstractVector, C::AbstractVector, x::AbstractVector) =
     clenshaw!(c, A, B, C, x, Ones{eltype(x)}(length(x)), x)
@@ -106,6 +109,9 @@ evaluates the orthogonal polynomial expansion with coefficients `c` at points `x
 where `A`, `B`, and `C` are `AbstractVector`s containing the recurrence coefficients
 as defined in DLMF.
 `x` may also be a single `Number`.
+
+If `c` is a matrix this treats each column as a separate vector of coefficients, returning a vector
+if `x` is a number and a matrix if `x` is a vector.
 """
 
 function clenshaw(c::AbstractVector, A::AbstractVector, B::AbstractVector, C::AbstractVector, x::Number)
