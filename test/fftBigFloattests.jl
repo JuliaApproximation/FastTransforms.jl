8be10bf3662309f6704f9823fc243e7e65e8ac33
MikaelSlevinsky <richard.slevinsky@umanitoba.ca>
2022-08-23 13:54:50 -0500

    remove old tests

--- test/fftBigFloattests.jl
+++
@@ -1,9 +0,0 @@
-using DSP, FastTransforms, Test
-
-@testset "BigFloat Convolution" begin
-    s = big(1) ./ (1:10)
-    s64 = Float64.(s)
-    @test Float64.(conv(s, s)) ≈ conv(s64, s64)
-    @test s == big(1) ./ (1:10) #67, ensure conv doesn't overwrite input
-    @test all(s64 .=== Float64.(big(1) ./ (1:10)))
-end
