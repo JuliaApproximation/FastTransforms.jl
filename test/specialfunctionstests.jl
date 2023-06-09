3b7996cd14db4a07251a881123ae119d85889046
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-06-09 21:01:23 +0530

    Fix pochhammer for a vector of n

--- test/specialfunctionstests.jl
+++ test/specialfunctionstests.jl
@@ -18,6 +18,8 @@ import FastTransforms: chebyshevmoments1, chebyshevmoments2, chebyshevjacobimome
     @test pochhammer(1.1,2.2) ≈ gamma(3.3)/gamma(1.1)
     @test pochhammer(-2,1) == pochhammer(-2,1.0) == pochhammer(-2.0,1) == -2
 
+    @test pochhammer(2,0:5) == [pochhammer(2,i) for i in 0:5]
+
     n = 0:1000
     λ = 0.125
     @test norm(Cnλ.(n, λ) ./ Cnλ.(n, big(λ)) .- 1, Inf) < 3eps()
